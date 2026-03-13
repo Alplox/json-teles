2857879b6dc2f3a44faa775b999fcc47d82de4cf
Alplox <42575576+Alplox@users.noreply.github.com>
2026-03-13 15:01:56 -0300

    Update convert.js

--- convert.js
+++ convert.js
@@ -5,22 +5,21 @@ const json = JSON.parse(fs.readFileSync("canales.json", "utf8"));
 let m3u = "#EXTM3U\n";
 
 for (const key in json) {
-  const canal = json[key];
+    const canal = json[key];
 
-  const nombre = canal.nombre || key;
-  const logo = canal.logo || "";
-  const categoria = canal.categoría || "General";
-  const pais = canal.país ? canal.país.toUpperCase() : "INTL";
+    const nombre = canal.nombre || key;
+    const logo = canal.logo || "";
+    const categoria = canal.categoría || "General";
+    const pais = canal.país ? canal.país.toUpperCase() : "INTL";
 
-  const urls = canal.señales?.m3u8_url || [];
+    const urls = canal.señales?.m3u8_url || [];
 
-  urls.forEach((url, i) => {
-    const numero = urls.length > 1 ? ` (${i + 1})` : "";
-    const nombreVisible = `${nombre}${numero} [${pais}]`;
-
-    m3u += `#EXTINF:-1 tvg-name="${nombre}" tvg-logo="${logo}" group-title="${categoria}",${nombreVisible}\n`;
-    m3u += `${url}\n`;
-  });
+    urls.forEach((url, i) => {
+        const numero = urls.length > 1 ? ` (${i + 1})` : "";
+        const nombreVisible = `${nombre}${numero} [${pais}]`;
+        m3u += `#EXTINF:-1 tvg-name="${nombre}" tvg-logo="${logo}" group-title="${categoria}", ${nombreVisible}\n`;
+        m3u += `${url}\n`;
+    });
 }
 
 fs.writeFileSync("canales.m3u", m3u);
